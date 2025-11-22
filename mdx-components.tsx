6679a288659ee944c78e54f3422aac83cf1c7fdb
goosewin <dan@goosewin.com>
2025-11-21 20:01:01 -0800

    fix: add missing components to MDXComponents

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,11 @@
 import type { MDXComponents } from 'mdx/types';
+import AnimatedRules from './app/components/blog/animated-rules';
+import PrincipleStatement from './app/components/blog/principle-statement';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    AnimatedRules,
+    PrincipleStatement,
   };
 }
