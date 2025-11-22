974c4846af68d73c7b4356893a5bc1646fca651a
goosewin <dan@goosewin.com>
2025-11-22 12:14:51 -0800

    feat: add more missing components to MDXComponents

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,15 @@
 import type { MDXComponents } from 'mdx/types';
+import Image from 'next/image';
 import AnimatedRules from './app/components/blog/animated-rules';
+import CommitmentCard from './app/components/blog/commitment-card';
 import PrincipleStatement from './app/components/blog/principle-statement';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    Image,
     AnimatedRules,
     PrincipleStatement,
+    CommitmentCard,
   };
 }
