f75d0758e04ddf686dddd603f2c6c4bb568457f7
Dan Goosewin <dan@goosewin.com>
2026-02-02 23:39:09 -0800

    chore(blog): remove mdx components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,15 +1,9 @@
 import type { MDXComponents } from 'mdx/types';
 import Image from 'next/image';
-import AnimatedRules from './app/components/blog/animated-rules';
-import CommitmentCard from './app/components/blog/commitment-card';
-import PrincipleStatement from './app/components/blog/principle-statement';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
     Image,
-    AnimatedRules,
-    PrincipleStatement,
-    CommitmentCard,
   };
 }
