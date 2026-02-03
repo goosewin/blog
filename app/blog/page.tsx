dba326796cab8ef5a8b9b91ee6e366e3fa6e2028
Dan Goosewin <dan@goosewin.com>
2026-02-02 23:35:31 -0800

    feat(blog): add blog index

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,17 @@
-import { redirect } from 'next/navigation';
+import { getAllBlogPosts } from '@/lib/blog';
+import BlogPostList from '@/app/components/blog-post-list';
+import SubscriptionForm from '@/app/components/subscription-form';
 
-export default function BlogIndexRedirect() {
-  redirect('/');
+export default async function Blog() {
+  const posts = await getAllBlogPosts();
+
+  return (
+    <div className="space-y-10">
+      <h1 className="text-3xl font-bold">Blog</h1>
+      <BlogPostList posts={posts} showDate={true} />
+      <div className="pt-6">
+        <SubscriptionForm />
+      </div>
+    </div>
+  );
 }
